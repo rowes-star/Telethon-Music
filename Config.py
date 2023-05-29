1755ec3c6d4fd695e5b413339bcff5c77bd00fc7
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2023-05-29 16:39:23 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,9 +8,9 @@ class Config(object):
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3d8ecee0ba7dddfc6fce4.jpg")
+    SUPPORT = os.environ.get("SUPPORT", "RQ_V0") # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "S0URCE_STAR") # Your Channel
+    START_IMG = os.environ.get("START_IMG", "https://graph.org/file/53ed6ce5f26457d998294.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
